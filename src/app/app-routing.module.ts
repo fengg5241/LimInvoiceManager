57ec271a655acc477db345d50c1fb8212bef1bdd
fengg5241 <fengg5241@users.noreply.github.com>
2019-04-02 17:09:59 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component'
+import {ProductsComponent} from './products/products.component'
+import {SalesComponent} from './sales/sales.component'
+import {CustomersComponent} from './customers/customers.component'
+import {UsersComponent} from './users/users.component'
+import {ReportsComponent} from './reports/reports.component'
+import {SettingsComponent} from './settings/settings.component'
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'sales', component: SalesComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
@@ -13,3 +25,4 @@ const routes: Routes = [
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
+

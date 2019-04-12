9d8992d8ac2b23d3445e99f62052afbd7869fff0
fengg5241 <fengg5241@users.noreply.github.com>
2019-04-12 17:40:17 +0800

    Update customer-upload.component.ts

--- src/app/customers/customer-upload/customer-upload.component.ts
+++ src/app/customers/customer-upload/customer-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LangService } from '../../lang.service';
 
 @Component({
   selector: 'app-customer-upload',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerUploadComponent implements OnInit {
 
-  constructor() { }
+  $page_title = 'Import Customers by CSV file';
+
+  constructor(private langService:LangService) { }
 
   ngOnInit() {
   }
+
+  lang(word){
+    return this.langService.lang(word);
+  }
+
+  checkfile(){}
 
 }

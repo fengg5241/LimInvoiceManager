ea2646d695b634b7353c1c568a39c812b4e6a2e8
fengg5241 <fengg5241@users.noreply.github.com>
2019-04-15 17:53:43 +0800

    Update system-setting.component.ts

--- src/app/settings/system-setting/system-setting.component.ts
+++ src/app/settings/system-setting/system-setting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LangService } from '../lang.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-system-setting',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemSettingComponent implements OnInit {
 
-  constructor() { }
+  $page_title = "System Settings";
+
+
+  constructor(private langService: LangService) { }
 
   ngOnInit() {
+    let thisObject = this;
+    $(document).ready(function () {
+      if ($('#protocol').val() == 'smtp') {
+        $('#smtp_config').slideDown();
+      } else if ($('#protocol').val() == 'sendmail') {
+        $('#sendmail_config').slideDown();
+      }
+      $('#protocol').change(function () {
+        if ($(this).val() == 'smtp') {
+          $('#sendmail_config').slideUp();
+          $('#smtp_config').slideDown();
+        } else if ($(this).val() == 'sendmail') {
+          $('#smtp_config').slideUp();
+          $('#sendmail_config').slideDown();
+        } else {
+          $('#smtp_config').slideUp();
+          $('#sendmail_config').slideUp();
+        }
+      });
+
+    });
   }
+
+  lang(word) {
+    return this.langService.lang(word);
+  }
+
 
 }

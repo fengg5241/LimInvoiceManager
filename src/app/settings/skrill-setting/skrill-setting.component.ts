c6e84684093d46a5a38dabae3fa6cb2c78d6a825
fengg5241 <fengg5241@users.noreply.github.com>
2019-04-22 17:43:15 +0800

    Update skrill-setting.component.ts

--- src/app/settings/skrill-setting/skrill-setting.component.ts
+++ src/app/settings/skrill-setting/skrill-setting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LangService } from '../../lang.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-skrill-setting',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkrillSettingComponent implements OnInit {
 
-  constructor() { }
+  $page_title = "Skrill Settings"
+
+  curSetting = {
+    active:0,
+    accountEmail:"",
+    secretWord:"",
+    fixedCharges:"",
+    extraChargesMy:"",
+    extraChargesOther:"",
+  }
+  constructor(private langService: LangService) { }
 
   ngOnInit() {
+    let thisObject = this;
+    $(document).ready(function () {
+      $('#active').change(function () {
+        var v = $(this).val();
+        if (v == 1) {
+          $('#account_email').attr('required', 'required');
+        } else {
+          $('#account_email').removeAttr('required');
+        }
+      });
+      var v = thisObject.curSetting.active;
+      if (v == 1) {
+        $('#account_email').attr('required', 'required');
+      } else {
+        $('#account_email').removeAttr('required');
+      }
+    });
   }
 
+  lang(word) {
+    return this.langService.lang(word);
+  }
 }

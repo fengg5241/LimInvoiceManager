702302df917ca2114df6185764cc9d03e112d530
fengg5241 <fengg5241@users.noreply.github.com>
2019-04-23 17:51:45 +0800

    Update tax-rate-setting.component.ts

--- src/app/settings/tax-rate-setting/tax-rate-setting.component.ts
+++ src/app/settings/tax-rate-setting/tax-rate-setting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LangService } from '../../lang.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-tax-rate-setting',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaxRateSettingComponent implements OnInit {
 
-  constructor() { }
+  $page_title = "Tax Rates";
+  $tax_rates: any;
+  
+  constructor(private langService: LangService) { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('#fileData').dataTable({
+        "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+        "aaSorting": [[1, "desc"]],
+        "iDisplayLength": 10,
+        "oTableTools": {
+          // "sSwfPath": "<?= $assets; ?>media/swf/copy_csv_xls_pdf.swf",
+          "aButtons": ["csv", "xls", { "sExtends": "pdf", "sPdfOrientation": "landscape", "sPdfMessage": "" }, "print"]
+        },
+        "aoColumns": [{ "bSortable": false }, null, null, null, { "bSortable": false }]
+      });
+    });
   }
 
+  lang(word) {
+    return this.langService.lang(word);
+  }
 }

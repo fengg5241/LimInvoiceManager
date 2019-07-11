f37f536b2ffae4ca63851a2213be31cf99195bc2
fengg5241 <fengg5241@users.noreply.github.com>
2019-07-11 17:52:23 +0800

    Update sales-report.component.ts

--- src/app/reports/sales-report/sales-report.component.ts
+++ src/app/reports/sales-report/sales-report.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LangService } from '../../lang.service';
+import { UtilService } from '../../common/util.service';
+import * as accounting from 'accounting/accounting.js';
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs';
+import 'datatables.net-buttons';
+import 'datatables.net-buttons/js/buttons.html5';
+import 'datatables.net-buttons/js/buttons.html5';
 
 @Component({
   selector: 'app-sales-report',
@@ -6,10 +16,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-report.component.css']
 })
 export class SalesReportComponent implements OnInit {
+  $page_title = 'Sales Report';
+  searchParams = {
+    pruductId: null,
+    customFields: null,
+    createdBy: null,
+    status: null,
+    start_date: null,
+    end_date: new Date().toISOString().split('T')[0]
+  };
 
-  constructor() { }
+  $settings: any;
+  $customers: [];
+  $users: [];
+  searchUser: null;
+  $total: 0;
+  $paid: 0;
+  $pp: 0;
+  $pending: 0;
+  $overdue: 0;
+  $cancelled: 0;
+  $tpp = {
+    total: 0,
+    paid: 0
+  };
+
+  constructor(
+    private langService: LangService,
+    private utilService: UtilService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
+    this.init();
   }
 
+  async init() {
+    let sysSettings = localStorage.getItem('LimSysSettings');
+    if (sysSettings) {
+      this.$settings = sysSettings;
+    } else {
+      let sysSettings1 = await this.http
+        .get('/api/sysSetting/selectAll')
+        .toPromise();
+      this.$settings = sysSettings1[0];
+    }
+    // this.$tax_rates = await this.http.get('/api/taxRate/selectAll').toPromise();
+  }
+
+  formatMoney(x, symbol) {
+    if (!symbol) {
+      symbol = '';
+    }
+    return accounting.formatMoney(
+      x,
+      symbol,
+      this.$settings.decimals,
+      this.$settings.thousandsSep == 0 ? ' ' : this.$settings.thousandsSep,
+      this.$settings.decimalsSep,
+      '%s%v'
+    );
+  }
 }

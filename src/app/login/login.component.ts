8493911eb1a53e50dea3759862f07af97210709d
fengg5241 <fengg5241@users.noreply.github.com>
2019-08-02 15:27:17 +0800

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { LangService } from '../lang.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private langService: LangService,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  lang(word) {
+    return this.langService.lang(word);
   }
-
 }
